6f4dda7e3777557d2c087f47aaf51219f3d67120
NandoApri26 <nandoapri@Apriandos-MacBook-Air.local>
2023-03-10 07:40:11 +0700

    Slicing Sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,22 @@ module.exports = {
       width: {
         '26' : '120px',
         '51' : '200px',
+        'search' : '261px',
+        'navbar': '1240px',
+      },
+      height: {
+        '18' : '72px',
       },
       margin: {
+        '2.5' : '10px',
+        '4.5' : '18px',
         '37' : '153px',
         '57' : '233px',
         '85' : '348px',
       },
       padding: {
+        '2.5' : '10px',
+        '4.5' : '18px',
         '11.5' : '46px',
         '37' : '153px',
         '57' : '233px',
@@ -29,9 +38,11 @@ module.exports = {
         "violet-1" : "#8265f7",
         "violet-hover" : "#6243e0",
         "dark-mode" : "#565656",
+        "search" : "#ECECEC",
       },
       boxShadow: {
-        "inner-2": "0 4px 15px -3px rgb(165 165 165 / 0.75), 0 4px 6px -4px rgb(165 165 165 / 0.75)"
+        "inner-2": "0 4px 15px -3px rgb(165 165 165 / 0.75), 0 4px 6px -4px rgb(165 165 165 / 0.75)",
+        "b" : "0 2px 5px -2px #8e8e8e",
       },
       fontFamily: {
         mulish : "Mulish",
